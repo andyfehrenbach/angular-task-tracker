90b461a32d76edc60bd7f2ed4a266ba6efd62aee
Andy Fehrenbach <andy.fehrenbach@gmail.com>
2021-08-25 12:15:42 -0500

    added in delete task service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs'
 import { Task } from '../Task';
-import { TASKS } from 'src/app/mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTasks(): Task[] {
-    return TASKS
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  DeleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
   }
 }
