15f457c67d7d1e1761ebb111e7f4c920285748eb
Andy Fehrenbach <andy.fehrenbach@gmail.com>
2021-08-25 14:25:37 -0500

    create ui service

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -26,4 +26,16 @@ export class TasksComponent implements OnInit {
     })
   }
 
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => {
+      this.tasks.push(task)
+    })
+
+  }
+
 }
